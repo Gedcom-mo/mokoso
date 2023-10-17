0ccfc9657d63975504dc984497cdfed4eef4f4ad
Gedcom-mo <148241239+Gedcom-mo@users.noreply.github.com>
2023-10-17 16:00:33 +0200

    De første filer til MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,53 @@
+input.onButtonPressed(Button.B, function () {
+    hånd = randint(0, 3)
+    if (hånd == 0) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (hånd == 1) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (hånd == 2) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+})
+let hånd = 0
+for (let index = 0; index < 11; index++) {
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        . . # . .
+        # # # # .
+        . . # . .
+        . # . . .
+        `)
+}
